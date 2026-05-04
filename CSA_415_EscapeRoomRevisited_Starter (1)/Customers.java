4e8fc0fc10ff925bfa9ce628c9533e9ef1c81b30
Maryna S <cyanlinemm@gmail.com>
2026-05-03 23:17:09 -0700

    Update Customers.java

--- CSA_415_EscapeRoomRevisited_Starter (1)/Customers.java
+++ CSA_415_EscapeRoomRevisited_Starter (1)/Customers.java
@@ -5,26 +5,48 @@ public class Customers{
   private final String[] cakes = {"vanilla ","chocolate "};
   private final String[] frostings = {"vanilla ","chocolate "};
   private final String[] endings = {" please",".."," maybe",", thanks"," or whatever",""};
+  private final String[] repliesP1 = {"Thank you so much!", "Thank you!", "Thanks.", ""};
+  private final String[] repliesP2 = {"Goodbye!", "Bye!", "Bye.", "See you next time!", ""};
   private String order="";
   private String words="";
+  private String reply="";
   
   public Customers(){}//will be implemented with the GUI
-  public String generateOrder(){
-    String order=cakes[(int)(Math.random()*cakes.length)]+"cake with "+
+  
+  public String generateOrder()
+  {
+    order=cakes[(int)(Math.random()*cakes.length)]+"cake with "+
     frostings[(int)(Math.random()*frostings.length)]+"frosting";
     
-    String words=greetings[(int)(Math.random()*greetings.length)]+
+    words=greetings[(int)(Math.random()*greetings.length)]+
     requests[(int)(Math.random()*requests.length)]+"a "+order+
     endings[(int)(Math.random()*endings.length)]+".";
     
     //words are saved just in case idk
+
     return words;
   }
-  public String getOrder(){
-    return order;
+
+  public String generateReply()
+  {
+    reply=repliesP1[(int)(Math.random()*repliesP1.length)]+" "+
+    repliesP2[(int)(Math.random()*repliesP2.length)]+".";
+
+    return reply;
   }
-  public static void main(String[] args){
-    Customers c = new Customers();
-    System.out.println(c.generateOrder());
+
+  public void nullifyOrder()
+  {
+    words = "";
   }
+
+  public String getOrder()
+  {
+    return words;
+  }
+//   public static void main(String[] args)
+//   {
+//     Customers c = new Customers();
+//     System.out.println(c.generateOrder());
+//   }
 }
