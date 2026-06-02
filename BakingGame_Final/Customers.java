27f5aea0aad51376598b4ff5eeb356e563f04fe2
Maryna S <cyanlinemm@gmail.com>
2026-06-01 23:47:54 -0700

    Update Customers.java

--- BakingGame_Final/Customers.java
+++ BakingGame_Final/Customers.java
@@ -3,36 +3,41 @@ public class Customers{
   private final String[] greetings = {"Good morning, ","...","Hi, ","Heyyy, ","Sup, ","Hello, ",""};
   private final String[] requests = {"I'd like ","could I get ","give me ","... umm, ",""};
   private final String[] products = {"vanilla cake","strawberry cake","chocolate cake","chocolate cupcake","cookie"};
-  //private final String[] frostings = {"vanilla ","chocolate "};
+  private final String[] frostings = {"vanilla ","strawberry ","chocolate "};
   private final String[] endings = {" please",".."," maybe",", thanks"," or whatever",""};
-  private final String[] repliesP1 = {"Thank you so much!", "Thank you!", "Thanks.", ""};
-  private final String[] repliesP2 = {"Goodbye!", "Bye!", "Bye.", "See you next time!", ""};
+  private final String[] Goodreplies = {"Thank you so much!", "Oh wow!", "This is my new favorite thing.", "I love you.",
+                                        "This is delicious!","I'd tip if money existed in this game.","Incredible!",
+                                        "Is this what a dream tastes like?","YAHOO","OMG!","This is the best thing ever!"};
+  private final String[] Badreplies = {"Eugh.", "Is this edible?", "What is this, cement?", "... You should concider changing professions.",
+                                        "... I don't have words for this...","Why are you tormenting me like this?",
+                                        "Are you trying to poison me?","I've never tasted anything worse."};
+  private final String[] repliesP2 = {"Goodbye!", "Bye!", "Bye.", "See you next time!", "","","",""};
   private String order="";
   private String words="";
   private String reply="";
   
-  public Customers(){}//will be implemented with the GUI
+  public Customers(){}
   
   public String generateOrder()
   {
-    order=products[(int)(Math.random()*products.length)];//+" with "
-    //frostings[(int)(Math.random()*frostings.length)]+"frosting";
-    
+    int rand = (int)(Math.random()*products.length);
+    order=products[rand];
+    if(rand<3)order+=" with "+frostings[(int)(Math.random()*frostings.length)]+"frosting";
     words=greetings[(int)(Math.random()*greetings.length)]+
     requests[(int)(Math.random()*requests.length)]+"a "+order+
     endings[(int)(Math.random()*endings.length)]+".";
     
-    //words are saved just in case idk
 
     return words;
   }
   public String getProduct(){
     return order;
   }
-  public String generateReply()
+  public String generateReply(int score)
   {
-    reply=repliesP1[(int)(Math.random()*repliesP1.length)]+" "+
-    repliesP2[(int)(Math.random()*repliesP2.length)]+".";
+    if(score>25)reply=Goodreplies[(int)(Math.random()*Goodreplies.length)]+" ";
+    else if(score<10)reply=Badreplies[(int)(Math.random()*Badreplies.length)]+" ";
+    reply+=repliesP2[(int)(Math.random()*repliesP2.length)];
 
     return reply;
   }
@@ -47,9 +52,5 @@ public class Customers{
   {
     return words;
   }
-//   public static void main(String[] args)
-//   {
-//     Customers c = new Customers();
-//     System.out.println(c.generateOrder());
-//   }
+  
 }
