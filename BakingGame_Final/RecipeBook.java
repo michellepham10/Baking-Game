e7531fee63b375ba877c0ca155d35b1c9166e9db
Maryna S <cyanlinemm@gmail.com>
2026-06-01 23:50:43 -0700

    Update RecipeBook.java

--- BakingGame_Final/RecipeBook.java
+++ BakingGame_Final/RecipeBook.java
@@ -1,29 +1,67 @@
 import javax.swing.*;
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
 public class RecipeBook extends JFrame{
-    String taText = "Vanilla cake batter: butter, eggs, flour, milk, sugar\n\n"+
-              "Strawberry cake batter: butter, eggs, flour, milk, strawberries, sugar\n\n"+
-              "Chocolate cake batter: butter, chocolate, eggs, flour, milk, sugar\n\n"+
-              "Chocolate cupcake batter: butter, chocolate, eggs, flour, milk, sourcream, sugar\n\n"+
-              "Cookie batter: butter, chocolate, eggs, flour, sugar\n\n";
-
-    public RecipeBook(){
+    private ImageIcon[] pages;
+    private int idx;
+    private JButton next;
+    private JButton previous;
+    private JPanel panel;
+    public RecipeBook() throws IOException{
+        idx=0;
+        pages = new ImageIcon[5];
+        
+        pages[0]=new ImageIcon(ImageIO.read(new File("1.png")));
+        pages[1]=new ImageIcon(ImageIO.read(new File("2.png")));
+        pages[2]=new ImageIcon(ImageIO.read(new File("3.png")));
+        pages[3]=new ImageIcon(ImageIO.read(new File("4.png")));
+        pages[4]=new ImageIcon(ImageIO.read(new File("5.png")));
         setTitle("Recipe Book");
-        setLocation(1500, 200);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation((int)(screenSize.width*0.02+1375), (int)(screenSize.height*0.2));
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JTextArea ta = new JTextArea(15, 20);
-        ta.setLineWrap(true);
-        ta.setWrapStyleWord(true);
-        ta.setText(taText);
-        ta.setEditable(false);
-        ta.setBackground(new Color(242, 222, 255));
-        ta.setFont(ta.getFont().deriveFont(25f));
-        JPanel panel = new JPanel();
-        panel.setBackground(new Color(188, 132, 224));
-        panel.add(ta);
-        getContentPane().add(panel);
-        pack();
-        setVisible(true);
+        setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+        panel = new JPanel();
+        panel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        
+        setSize(new Dimension(512,662));
+        JLabel label = new JLabel();
+        label.setIcon(pages[0]);
+        label.setAlignmentX(Component.LEFT_ALIGNMENT);
+        label.setPreferredSize(new Dimension(512,512));
+        next = new JButton("Next");
+        next.setFont(new Font("Arial", Font.BOLD, 24));
+        next.setAlignmentX(Component.LEFT_ALIGNMENT);
+        next.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(idx<4){
+                    idx++;
+                    label.setIcon(pages[idx]);
+                }
+            }
+        });
+        previous = new JButton("Previous");
+        previous.setFont(new Font("Arial", Font.BOLD, 24));
+        previous.setAlignmentX(Component.LEFT_ALIGNMENT);
+        previous.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(idx>0){
+                    idx--;
+                    label.setIcon(pages[idx]);
+                }
+            }
+        });
+        add(label);
+        panel.add(previous);
+        panel.add(next);
+        add(panel);
+        //pack();
     }
 }
 
